3e70644b9c39405aea9a91627ddeb929d91f7621
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-10-02 13:54:53 +0200

    update working version (need to add exceptions for point out of range

--- dijkstra.py
+++ dijkstra.py
@@ -1,97 +1,67 @@
-from collections import deque, namedtuple
+def dijkstra(graph,position,dest,visited=[],distances={},predecessors={}):
+    
+    # On verifie si les 2 points sont dans notre réseau
+    if position not in graph:
+        print('Le point de départ n\'existe pas.')
+    if dest not in graph:
+        print('Le point d\'arrivé n\'existe pas.')  
+    if position != dest:
+        # On commence en mettant le point de départ à 0
+        if not visited: 
+            distances[position]=0
+        # Puis nous visitons les points voisins pour calculer leurs distances 
+        for neighbor in graph[position] :
+            if neighbor not in visited:
+                new_distance = distances[position] + graph[position][neighbor]
+                if new_distance < distances.get(neighbor,float('inf')):
+                    distances[neighbor] = new_distance
+                    predecessors[neighbor] = position
+        # On marque les points voisins comme étant visités
+        visited.append(position)
+        # Maintenant que les points voisins sont visités, on choisit le prochain point avec le poids le plus bas.
+
+        unvisited={}   
+        for p in graph:
+            if p not in visited:
+                unvisited[p] = distances.get(p,float('inf'))        
+        dijkstra(graph,min(unvisited, key=unvisited.get),dest,visited,distances,predecessors)
+    else :
+        # Maintenant que tous les points dans le réseau sont visités, on obtient le chemin 
+        path=[]
+        pred=dest
+        while pred != None:
+            path.append(pred)
+            pred=predecessors.get(pred,None)
+        path.reverse()
+        print('Le chemin le plus court : '+str(path)+" avec une distance de "+str(distances[dest]))
 
 
-# we'll use infinity as a default distance to nodes.
-inf = float('inf')
-Edge = namedtuple('Edge', 'start, end, cost')
-
-
-def make_edge(start, end, cost=1):
-  return Edge(start, end, cost)
-
-
-class Graph:
-    def __init__(self, edges):
-        # let's check that the data is right
-        wrong_edges = [i for i in edges if len(i) not in [2, 3]]
-        if wrong_edges:
-            raise ValueError('Wrong edges data: {}'.format(wrong_edges))
-
-        self.edges = [make_edge(*edge) for edge in edges]
-
-    @property
-    def vertices(self):
-        return set(
-            sum(
-                ([edge.start, edge.end] for edge in self.edges), []
-            )
-        )
-
-    def get_node_pairs(self, n1, n2, both_ends=True):
-        if both_ends:
-            node_pairs = [[n1, n2], [n2, n1]]
-        else:
-            node_pairs = [[n1, n2]]
-        return node_pairs
-
-    def remove_edge(self, n1, n2, both_ends=True):
-        node_pairs = self.get_node_pairs(n1, n2, both_ends)
-        edges = self.edges[:]
-        for edge in edges:
-            if [edge.start, edge.end] in node_pairs:
-                self.edges.remove(edge)
-
-    def add_edge(self, n1, n2, cost=1, both_ends=True):
-        node_pairs = self.get_node_pairs(n1, n2, both_ends)
-        for edge in self.edges:
-            if [edge.start, edge.end] in node_pairs:
-                return ValueError('Edge {} {} already exists'.format(n1, n2))
-
-        self.edges.append(Edge(start=n1, end=n2, cost=cost))
-        if both_ends:
-            self.edges.append(Edge(start=n2, end=n1, cost=cost))
-
-    @property
-    def neighbours(self):
-        neighbours = {vertex: set() for vertex in self.vertices}
-        for edge in self.edges:
-            neighbours[edge.start].add((edge.end, edge.cost))
-
-        return neighbours
-
-    def dijkstra(self, source, dest):
-        assert source in self.vertices, 'Such source node doesn\'t exist'
-        distances = {vertex: inf for vertex in self.vertices}
-        previous_vertices = {
-            vertex: None for vertex in self.vertices
-        }
-        distances[source] = 0
-        vertices = self.vertices.copy()
-
-        while vertices:
-            current_vertex = min(
-                vertices, key=lambda vertex: distances[vertex])
-            vertices.remove(current_vertex)
-            if distances[current_vertex] == inf:
-                break
-            for neighbour, cost in self.neighbours[current_vertex]:
-                alternative_route = distances[current_vertex] + cost
-                if alternative_route < distances[neighbour]:
-                    distances[neighbour] = alternative_route
-                    previous_vertices[neighbour] = current_vertex
-
-        path, current_vertex = deque(), dest
-        while previous_vertices[current_vertex] is not None:
-            path.appendleft(current_vertex)
-            current_vertex = previous_vertices[current_vertex]
-        if path:
-            path.appendleft(current_vertex)
-        return path
-
-
-graph = Graph([
-    ("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
-    ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
-    ("e", "f", 9)])
-
-print(graph.dijkstra("a", "e"))
+    
+start = input('Point de départ : ').lower()
+dest = input ('Point d\'arrivé : ').lower()
+# Le réseau est donné sous la forme de "graph"
+graph = {
+    'a':{
+        'b':2,
+        'c':5
+    },
+    'b':{
+        'e':2
+    },
+    'c':{
+        'e':1,
+        'b':2
+    },
+    'd':{
+        'c':4,
+        'b':1
+    },
+    'e':{
+        'f':2,
+        'd':4
+    },
+    'f':{
+        'd':1
+    }
+}
+dijkstra(graph,start,dest)

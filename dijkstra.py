f128ce819302abeb00f75672bd6ffb1c7c19564e
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-10-03 20:00:45 +0200

    update

--- dijkstra.py
+++ dijkstra.py
@@ -1,15 +1,14 @@
 def dijkstra(graph,startPoint,endPoint,visited=[],distances={},predecessors={}):
-    
-    # On verifie si les 2 points sont dans notre réseau
+    # Est-ce que le point de départ et le poinr d'arrivé appartiennent au graphe ?
     if startPoint not in graph:
         print('Le point de départ n\'existe pas.')
     if endPoint not in graph:
         print('Le point d\'arrivé n\'existe pas.')  
     if startPoint != endPoint:
-        # On commence en mettant le point de départ à 0
+        # On met le poids du premier point à 0
         if not visited: 
             distances[startPoint]=0
-        # Puis nous visitons les points voisins pour calculer leurs distances 
+        # On visite les successeurs pour calculer la distance
         for neighbor in graph[startPoint] :
             if neighbor not in visited:
                 newDistance = distances[startPoint] + graph[startPoint][neighbor]
@@ -18,50 +17,42 @@ def dijkstra(graph,startPoint,endPoint,visited=[],distances={},predecessors={}):
                     predecessors[neighbor] = startPoint
         # On marque les points voisins comme étant visités
         visited.append(startPoint)
-        # Maintenant que les points voisins sont visités, on choisit le prochain point avec le poids le plus bas.
-
+        # Parmis les points visités, on choisit celui avec le poids le plus bas
         unvisited={}   
         for p in graph:
             if p not in visited:
                 unvisited[p] = distances.get(p,float('inf'))        
         dijkstra(graph,min(unvisited, key=unvisited.get),endPoint,visited,distances,predecessors)
     else :
-        # Maintenant que tous les points dans le réseau sont visités, on obtient le chemin 
+        # Une fois tout les points visités, on liste le meilleur chemin de routage
         path=[]
         pred=endPoint
         while pred != None:
             path.append(pred)
             pred=predecessors.get(pred,None)
-        path.reverse()
+        # path.reverse()
         print("Le chemin le plus court : {} avec une distance de {} ".format(path, distances[endPoint]))
 
-
-    
 startPoint = input('Point de départ : ').lower()
 destinationPoint = input ('Point d\'arrivé : ').lower()
-# Le réseau est donné sous la forme de "graph"
+# Le graphe est sous la forme d'un dictionnaire comportant les points du graphes comportant eux-même un dictionnaire de leur successeurs avec leur poids
 graph = {
-    'a':{
-        'b':2,
-        'c':5
+    '1':{
+        '2':10,
+        '4': 30,
+        '5':100
     },
-    'b':{
-        'e':2
+    '2':{
+        '3':50
     },
-    'c':{
-        'e':1,
-        'b':2
+    '3':{
+        '5':10,
     },
-    'd':{
-        'c':4,
-        'b':1
+    '4':{
+        '3':20,
+        '5':60
     },
-    'e':{
-        'f':2,
-        'd':4
-    },
-    'f':{
-        'd':1
+    '5':{
     }
 }
 dijkstra(graph,startPoint,destinationPoint)
